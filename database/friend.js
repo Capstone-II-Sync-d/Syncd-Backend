3229786136fede89803dd3617c38d8144c46be93
Guarotav <guarotav@gmail.com>
2025-07-30 14:51:56 -0400

    #4 Created model for Friend

---
+++ database/friend.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require("sequelize");
+const db = require("./db");
+
+const Friend = db.define("friend", {
+  user1: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+  },
+  user2: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+  },
+});
+
+module.exports = Friend;
