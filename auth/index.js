4c6f019b9f07d05f38a4dad4476326692c41a2ac
Joseph Collado <sourpina@gmail.com>
2025-07-30 10:57:58 -0400

    Added environment variable for cookie sameSite property

--- auth/index.js
+++ auth/index.js
@@ -82,7 +82,7 @@ router.post("/auth0", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.SAME_SITE || "strict",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     });
 
@@ -143,7 +143,7 @@ router.post("/signup", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: process.env.SAME_SITE || "strict",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     });
 
@@ -194,7 +194,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.SAME_SITE || "strict",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     });
 
