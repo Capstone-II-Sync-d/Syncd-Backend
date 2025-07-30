c27d54293559ffa4b5b835c0455af856cb8531d2
Nuria Siddiqa <nuri20024@gmail.com>
2025-07-30 14:51:27 -0400

    #6 Fixed errors in calendarItem.js

--- database/calendarItem.js
+++ database/calendarItem.js
@@ -4,6 +4,7 @@ const db = require("./db");
 const CalendarItem = db.define("calendar_item", {
     title: {
         type: DataTypes.STRING,
+        allowNull: false,
     },
     description: {
         type: DataTypes.STRING,
@@ -12,23 +13,28 @@ const CalendarItem = db.define("calendar_item", {
         type: DataTypes.STRING
     },
     start: {
-        type: DataTypes.DATETIME,
+        type: DataTypes.DATE,
+        allowNull: false,
     },
     end: {
-        type: DataTypes.DATETIME,
+        type: DataTypes.DATE,
+        allowNull: false,
     },
     chatLink: {
         type: DataTypes.STRING,
+        allowNull: true,
     },
     itemType: {
         type: DataTypes.ENUM[('personal','event')],
+        allowNull: false,
     },
     privacy: {
         type: DataTypes.ENUM[('public','private')],
+        defaultValue: 'private',
+        allowNull: false,
     },
     businessId:  {
         type: DataTypes.INTEGER,
-        allowNull: true,
         references: {
             model: 'businesses',
             key: 'id',
