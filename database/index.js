48157fdd5eb9bb73d9bdad34b3d226549ceb8b14
Nuria Siddiqa <nuri20024@gmail.com>
2025-07-31 11:18:25 -0400

    Resolved conflict

--- database/index.js
+++ database/index.js
@@ -1,6 +1,5 @@
 const db = require("./db");
 const User = require("./user");
-<<<<<<< Updated upstream
 const Business = require("./business");
 const CalendarItem = require("./calendarItem");
 const FriendRequest = require("./friendRequest");
@@ -41,87 +40,14 @@ CalendarItem.hasMany(Attendee, {
 });
 Attendee.belongsTo(CalendarItem, {
   foreignKey: 'eventId'
-=======
-const Friend = require("./friend");
-const Follower = require("./follower");
-const Attendee = require("./attendee");
-const Business = require("./business");
-const CalendarItem = require("./calendarItem");
-const Reminder = require("./reminder");
-
-// ---------------------------------------------------------------------
-//User owns a business
-User.hasMany(Business, {
-  foreignKey: "ownerId",
-});
-Business.belongsTo(User, {
-  foreignKey: "ownerId",
-});
-
-// ---------------------------------------------------------------------
-//User has CalendarItems
-User.hasMany(CalendarItem, {
-  foreignKey: "ownerId",
-});
-CalendarItem.belongsTo(User, {
-  foreignKey: "ownerId",
-});
-
-//Business has calendar items
-Business.hasMany(CalendarItem, {
-  foreignKey: "businessId",
-});
-CalendarItem.belongsTo(Business, {
-  foreignKey: "businessId",
-});
-
-// ---------------------------------------------------------------------
-//CalendarItem has many saves from Users
-CalendarItem.hasMany(Attendee, {
-  foreignKey: "eventId",
-});
-Attendee.belongsTo(CalendarItem, {
-  foreignKey: "ownerId",
-});
-
-Attendee.belongsTo(User, {
-  foreignKey: "userId",
-});
-
-// ---------------------------------------------------------------------
-//Follower Associations
-Business.hasMany(Follower, {
-  foreignKey: "businessId",
-});
-Follower.belongsTo(Business, {
-  foreignKey: "businessId",
-});
-
-//User follows many businesses
-User.hasMany(Follower, {
-  foreignKey: "userId",
-});
-Follower.belongsTo(Business, {
-  foreignKey: "userId",
->>>>>>> Stashed changes
 });
 
 module.exports = {
   db,
   User,
-<<<<<<< Updated upstream
   Business,
   CalendarItem,
   FriendRequest,
   Friend,
   Follower,
 };
-=======
-  Friend,
-  Follower,
-  Attendee,
-  Business,
-  CalendarItem,
-  Reminder,
-};
->>>>>>> Stashed changes
