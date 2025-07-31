f13a025df98cff768157f8db3de84dcbc2c96b9e
Guarotav <guarotav@gmail.com>
2025-07-31 14:30:38 -0400

    Created Routing For editing user profile, deleting user, getting all friendships of a user

--- api/users.js
+++ api/users.js
@@ -1,1 +1,82 @@
 const express = require("express");
+const router = express.Router();
+//Importing models needed
+const { User, Business, FriendShip, Follows } = reuire("../database");
+// Import Sequelize operators
+const { Op } = require("sequelize");
+
+//User Profile Routing
+//|-----------------------------------------------------------------|
+
+//Edit a users profile information
+router.patch("/user-edit/:id", async (req, res) => {
+  try {
+    //Get user ID from url parameters
+    const userId = req.params.id;
+
+    //Find the user with the id inside url params
+    const user = await User.findByPk(userId);
+
+    //Get the body of information sent.
+    //If anything was changed, it will be updated.
+    const userProfile = req.body;
+
+    //Updates the user profile with body sent in.
+    await user.update(userProfile);
+  } catch (error) {
+    console.error("Error editing user profile:", error);
+    res.status(500).json({ error: "Failed to edit user profile" });
+  }
+});
+
+//Delete a users profile
+router.delete("/user-delete/:id", async (req, res) => {
+  try {
+    //Get user ID from url parameters
+    const userId = req.params.id;
+
+    //Find the user with the id inside url params
+    const user = await User.findByPk(userId);
+
+    //Deletes that user
+    await user.delete();
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).json({ error: "Failed to delete user" });
+  }
+});
+
+//Get all friendships of a user
+router.get("/user-friendShip/:id", async (req, res) => {
+  try {
+    //Get user ID from url parameters
+    const userId = req.params.id;
+
+    const friends = await FriendShip.findAll({
+      [Op.or]: [
+        {
+          user1: userId,
+          user2: userId,
+        },
+      ],
+    });
+    res.status(200).json(friends);
+  } catch (error) {
+    console.error("Error detching friendships:", error);
+    res.status(500).json({ error: "Failed to fetch friendships" });
+  }
+});
+
+//Get all businesses made by a user
+router.post("/", async (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error detching businesses made by user:", error);
+    res.status(500).json({ error: "Failed to fetch businesses" });
+  }
+});
+
+//Get all businesses a user follows
+router.post("/", async (req, res) => {});
+
+//Business Profile Routing
