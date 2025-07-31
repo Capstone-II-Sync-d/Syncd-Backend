d4c4d00b000d523155db17a85ffb9ec8074485af
Nuria Siddiqa <nuri20024@gmail.com>
2025-07-31 11:26:41 -0400

    #6 - Fixed error in calendaritem.js

--- database/calendarItem.js
+++ database/calendarItem.js
@@ -2,51 +2,14 @@ const { DataTypes } = require("sequelize");
 const db = require("./db");
 
 const CalendarItem = db.define("calendar_item", {
-<<<<<<< Updated upstream
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    description: {
-        type: DataTypes.STRING,
-    },
-    location: {
-        type: DataTypes.STRING
-    },
-    start: {
-        type: DataTypes.DATE,
-        allowNull: false,
-    },
-    end: {
-        type: DataTypes.DATE,
-        allowNull: false,
-    },
-    chatLink: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    itemType: {
-        type: DataTypes.ENUM[('personal','event')],
-        allowNull: false,
-    },
-    privacy: {
-        type: DataTypes.ENUM[('public','private')],
-        defaultValue: 'private',
-        allowNull: false,
-    },
-    businessId:  {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'businesses',
-            key: 'id',
-        }
-    },
-    userId: {
-        type: DataTypes. INTEGER,
-        allowNull: false,
-=======
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
   title: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
   description: {
     type: DataTypes.STRING,
@@ -55,27 +18,31 @@ const CalendarItem = db.define("calendar_item", {
     type: DataTypes.STRING,
   },
   start: {
-    type: DataTypes.DATETIME,
+    type: DataTypes.DATE,
+    allowNull: false,
   },
   end: {
-    type: DataTypes.DATETIME,
+    type: DataTypes.DATE,
+    allowNull: false,
   },
   chatLink: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   itemType: {
     type: DataTypes.ENUM[("personal", "event")],
+    allowNull: false,
   },
   privacy: {
     type: DataTypes.ENUM[("public", "private")],
+    defaultValue: "private",
+    allowNull: false,
   },
   businessId: {
     type: DataTypes.INTEGER,
-    allowNull: true,
     references: {
       model: "businesses",
       key: "id",
->>>>>>> Stashed changes
     },
   },
   userId: {
