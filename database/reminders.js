3205b982f13f223ae1739cc2f180e4573cc069cb
Mekhribon Yusufbekova <mmyusufbekova@gmail.com>
2025-07-30 23:10:41 -0400

    Created Reminders Model

--- database/reminders.js
+++ database/reminders.js
@@ -1,2 +1,28 @@
 const { DataTypes } = require("sequelize");
 const db = require("./db");
+
+const Reminder = db.define("reminder", {
+  timeValue: {
+    type: DataTypes.INTEGER,
+    validate: {
+      isTimePositive(timeValue) {
+        if (timeValue <= 0) {
+          throw new Error("Time value must be positive");
+        }
+      },
+    },
+  },
+  timeScale: {
+    type: DataTypes.ENUM[("minutes", "hours", "days", "weeks")],
+  },
+  calendarItemId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  ownerId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+module.exports = Reminder;
