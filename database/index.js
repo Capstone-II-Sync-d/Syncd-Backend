6574e9c927e073120f26063b6efa0e17c3bcb457
Nuria Siddiqa <nuri20024@gmail.com>
2025-07-30 23:40:12 -0400

    #9 - created associations in index.js, also added Business and CalendarItem model to export

--- database/index.js
+++ database/index.js
@@ -1,11 +1,52 @@
 const db = require("./db");
 const User = require("./user");
+const Business = require("./business");
+const CalendarItem = require("./calendarItem");
 const FriendRequest = require("./friendRequest");
 const Friend = require("./friend");
 const Follower = require("./follower");
+
+// -------------- associations -----------------//
+
+User.hasMany(CalendarItem, {
+  foreignKey: 'userId'
+});
+User.hasMany(Business, {
+  foreignKey: 'ownerID'
+});
+Business.belongsTo(User, {
+  foreignKey: 'userId'
+});
+User.hasMany(Friend, {
+  foreignKey:'userId'
+});
+Business.hasMany(CalendarItem, {
+  foreignKey: 'businessId'
+});
+Business.hasMany(Follower, {
+  foreignKey: 'businessId'
+});
+Follower.belongsTo(Business, {
+  foreignKey: 'businessId'
+});
+User.hasMany(Follower, {
+  foreignKey: 'userId'
+});
+Follower.belongsTo(User, {
+  foreignKey: 'userId'
+});
+CalendarItem.hasMany(Attendee, {
+  foreignKey: 'eventId'
+});
+Attendee.belongsTo(CalendarItem, {
+  foreignKey: 'eventId'
+});
+
 module.exports = {
   db,
   User,
+  Business,
+  CalendarItem,
   FriendRequest,
   Friend,
   Follower,
