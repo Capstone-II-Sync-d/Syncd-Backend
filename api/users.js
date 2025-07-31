47b49cba7556185d110c95dd9e3141da782baa31
Guarotav <guarotav@gmail.com>
2025-07-31 14:49:26 -0400

    All routing created for Users profiles as well as added descriptive comments throughout

--- api/users.js
+++ api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 //Importing models needed
-const { User, Business, FriendShip, Follows } = reuire("../database");
+const { User, Business, FriendShip, Follow } = reuire("../database");
 // Import Sequelize operators
 const { Op } = require("sequelize");
 
@@ -23,6 +23,7 @@ router.patch("/user-edit/:id", async (req, res) => {
 
     //Updates the user profile with body sent in.
     await user.update(userProfile);
+    //Send a status of 200 if it goes through and send a json message saying what the 200 message was for
     res.status(200).json("Profile has successfully updated");
   } catch (error) {
     console.error("Error editing user profile:", error);
@@ -41,6 +42,7 @@ router.delete("/user-delete/:id", async (req, res) => {
 
     //Deletes that user
     await user.delete();
+    //Send a status of 200 if it goes through and send a json message saying what the 200 message was for
     res.status(200).json("User has successfully been deleted");
   } catch (error) {
     console.error("Error deleting user:", error);
@@ -54,8 +56,10 @@ router.get("/user-friendShip/:id", async (req, res) => {
     //Get user ID from url parameters
     const userId = req.params.id;
 
+    //Find all friend of that specific user
     const friends = await FriendShip.findAll({
       where: {
+        //or operator in express to check for any friendship where user1 OR user2 has that specific userId
         [Op.or]: [
           {
             user1: userId,
@@ -64,6 +68,7 @@ router.get("/user-friendShip/:id", async (req, res) => {
         ],
       },
     });
+    //Send back status of 200 if everything goes through and send the friends of that user
     res.status(200).json(friends);
   } catch (error) {
     console.error("Error detching friendships:", error);
@@ -77,8 +82,10 @@ router.get("/user-businesseses-made/:id", async (req, res) => {
     //Get user ID from url parameters
     const userId = req.params.id;
 
+    //Find all business by this owner
     const businesses = Business.findAll({ where: { ownerId: userId } });
 
+    //Send back status of 200 if everything goes through and send the friends of that user
     res.status(200).json(businesses);
   } catch (error) {
     console.error("Error detching businesses made by user:", error);
@@ -87,6 +94,12 @@ router.get("/user-businesseses-made/:id", async (req, res) => {
 });
 
 //Get all businesses a user follows
-router.post("/user-follows/:id", async (req, res) => {});
+router.post("/user-follows/:id", async (req, res) => {
+  //Get user ID from url parameters
+  const user_id = req.params.id;
 
+  const businessFollowing = Follow.findAll({ where: { userId: user_id } });
+});
+
+//|---------------------------------------------------------------------------------|
 //Business Profile Routing
