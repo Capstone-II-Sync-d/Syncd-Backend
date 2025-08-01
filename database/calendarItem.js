d15d41d967b4e8f6b75804a66c2274b54c1799c5
Joseph Collado <sourpina@gmail.com>
2025-08-01 01:01:07 -0400

    #6 Refactored into CalendarItem and Event
    An Event is a Calendar Item with extra information. Businesses cannot create pure Calendar Items and must create them through an Event

--- database/calendarItem.js
+++ database/calendarItem.js
@@ -14,22 +14,10 @@ const CalendarItem = db.define("calendar_item", {
   description: {
     type: DataTypes.STRING,
     allowNull: true,
-    validate: {
-      notNullForEvent(value) {
-        if (value === null && this.itemType !== "personal")
-          throw new Error("Decription cannot be null unless it's a personal calendar item");
-      }
-    },
   },
   location: {
     type: DataTypes.STRING,
     allowNull: true,
-    validate: {
-      notNullForEvent(value) {
-        if (value === null && this.itemType !== "personal" && this.privacy !== "private")
-          throw new Error("Location cannot be null unless it's a private personal calendar item");
-      }
-    },
   },
   start: {
     type: DataTypes.DATE,
@@ -39,34 +27,10 @@ const CalendarItem = db.define("calendar_item", {
     type: DataTypes.DATE,
     allowNull: false,
   },
-  chatLink: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  itemType: {
-    type: DataTypes.ENUM(["personal", "event"]),
-    allowNull: false,
-    validate: {
-      notPersonalForBusiness(value) {
-        if (this.businessId !== null && value === "personal")
-          throw new Error("Item Type cannot be 'personal' for businesses");
-      }
-    },
-  },
   privacy: {
     type: DataTypes.ENUM(["public", "private"]),
     defaultValue: "private",
     allowNull: false,
-  },
-  businessId: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    validate: {
-      notNullForEvent(value) {
-        if (value === null && this.itemType !== "personal")
-          throw new Error("BusinessID cannot be null unless it's a personal calendar item");
-      }
-    },
   },
   userId: {
     type: DataTypes.INTEGER,
