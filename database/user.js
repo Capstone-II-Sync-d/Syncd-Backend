8fad361c0ca2c1102588f757bec40fe085ef5467
Joseph <sourpina@gmail.com>
2025-08-01 14:17:35 -0400

    #1 Changed allowNull on profile picture to be false

--- database/user.js
+++ database/user.js
@@ -46,7 +46,7 @@ const User = db.define("user", {
   },
   profilePicture: {
     type: DataTypes.STRING,
-    allowNull: true,
+    allowNull: false,
     defaultValue:
       "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg",
   },
