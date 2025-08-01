e5ca523252f71342a6d836c6220b13ab27f4db13
Nuria Siddiqa <nuri20024@gmail.com>
2025-08-01 14:53:40 -0400

    #12 - created route to get a specific calendar item by id

--- api/calendarItem.js
+++ api/calendarItem.js
@@ -25,15 +25,7 @@ router.get("/user/:id/calendaritems", async (req, res) => {
     const userCalendarItems = await CalendarItem.findAll({
       // Filter so we only get calendar items created by this specific user
       where: { userId: id },
-      // Load user information as well
-      include: [
-        {
-          model: User,
-          // Only get these specific fields from the user table
-          attributes: ["id", "firstName", "lastName", "username"],
-        },
-      ],
-      //Sort by start time (earliest first)
+      // Sort by start time (earliest first)
       order: [["start", "ASC"]],
     });
     // Send success response if everything works and send back the user's calendar items
@@ -47,11 +39,31 @@ router.get("/user/:id/calendaritems", async (req, res) => {
 });
 
 //|-----------------------------------------------------------------|
-// Get a specific calendar item by id for a specific user
+// Get a specific calendar item by id
+router.get("/calendaritems/:itemId", async (req, res) => {
+  // Get calendar item id from parameters
+  const itemId = req.params.itemId;
+  try {
+    // Find this specific calendar item
+    const calendarItem = await CalendarItem.findOne({
+      // Filter so we only recieve this specific item
+      where: { id: itemId },
+    });
+    // Check if calendar item was found, send error message if !found
+    if (!calendarItem) {
+      return res.status(404).json({ error: "Calendar item not found" });
+    }
+    // Send success if everything is good and return the calendar item
+    res.status(200).json(calendarItem);
+  } catch (error) {
+    console.error("Error fetching calendar item: ", error);
+    res.status(500).json({ error: `Failed to fetch calendar item ${itemId}` });
+  }
+});
 
 //|-----------------------------------------------------------------|
 // Create a new calendar item for a user
-
+router.post("usr");
 //|-----------------------------------------------------------------|
 // Edit a user calendar item by id
 
@@ -79,4 +91,4 @@ router.get("/user/:id/calendaritems", async (req, res) => {
 
 //|-------------------------------------------------------------------|
 
-modeul.exports = router;
+module.exports = router;
