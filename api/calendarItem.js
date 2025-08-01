3d64fa6b6900b30152bb4fe102e2696aad40ffca
Nuria Siddiqa <nuri20024@gmail.com>
2025-08-01 15:55:42 -0400

    #12 - Finished most of the routing for user calendaritem api

--- api/calendarItem.js
+++ api/calendarItem.js
@@ -40,16 +40,13 @@ router.get("/user/:id/calendaritems", async (req, res) => {
 });
 
 //|-----------------------------------------------------------------|
-// Get a specific calendar item by id
+// Get one of a user's calendar items id
 router.get("/calendaritems/:itemId", async (req, res) => {
   // Get calendar item id from parameters
   const itemId = req.params.itemId;
   try {
     // Find this specific calendar item
-    const calendarItem = await CalendarItem.findOne({
-      // Filter so we only recieve this specific item
-      where: { id: itemId },
-    });
+    const calendarItem = await CalendarItem.findByPk(itemId);
 
     // Check if calendar item was found, send error message if !found
     if (!calendarItem) {
@@ -70,15 +67,16 @@ router.post("/user/:id/calendaritems", async (req, res) => {
   // Get user id from path
   const userId = req.params.id;
   try {
-    // Get calendar event infomration from request body
+    // Get calendar item information from request body
     const calendarItemData = req.body;
-    // Create new calendar event with the user id attached
+
+    // Create new calendar item with the user id attached
     const newCalendarItem = await CalendarItem.create({
       ...calendarItemData,
       userId: parseInt(userId),
     });
 
-    // Return success message with the  new calendar item
+    // Return success message with the new calendar item
     res.status(201).json(newCalendarItem);
   } catch (error) {
     console.error("Error creating calendar item: ", error);
@@ -88,9 +86,55 @@ router.post("/user/:id/calendaritems", async (req, res) => {
 
 //|-----------------------------------------------------------------|
 // Edit a user calendar item by id
+router.patch("/calendaritems/:itemId", async (req, res) => {
+  // Get calendar item id from url
+  const itemId = req.params.itemId;
+  try {
+    // Get the updated information from the request body
+    const updatedData = req.body;
+    // Find the specific calendar item with that id
+    const calendarItem = await CalendarItem.findByPk(itemId);
+
+    // Check if calendar item was found, send message if not
+    if (!calendarItem) {
+      return res.status(404).json({ error: "Calendar item not found"});
+    }
+
+    // Update the calendar item with the new information
+    await calendarItem.update(updatedData);
+
+    // Send success message with the updated calendar item
+    res.status(200).json(calendarItem);
+  } catch (error) {
+    console.error("Error editing calendar item: ", error);
+    res.status(500).json({error: "Failed to edit calendar item"});
+  }
+});
 
 //|-----------------------------------------------------------------|
 // Delete a user calendar item by id
+router.delete("/calendaritems/:itemId", async (req,res) => {
+  // Get calendar item id from URL
+  const itemId = req.params.itemId;
+  try {
+    // Find the calendar item with that id
+    const calendarItem = await CalendarItem.findByPk(itemId);
+
+    // Check if calendar item was found, send error message if !found
+    if (!calendarItem) {
+      return res.status(404).json({ error: "Calendar item not found"});
+    }
+
+    // Delete the item
+    await calendarItem.destroy();
+
+    // Send success message
+    res.status(200).json9({message: "Calendar item deleted"});
+  } catch (error) {
+    console.error("Error deleting calendar item:", error);
+    res.status(500).json({error: "Failed to delete calendar item"});
+  }
+});
 
 //|=====================================================================================|
 //       **************|Business Caldendar Item Routing (Bonnie)|****************
