fe51c601f3f2dca7b5a7571f08afc00c14f02337
Guarotav <guarotav@gmail.com>
2025-08-07 13:35:44 -0400

    #18 Added an event listener for when a user sends a friendrequest or accepts a friend request. Sill needs testing but logic seems to be correct. Am blocked without user search at the moment.

--- socket-server.js
+++ socket-server.js
@@ -16,7 +16,7 @@ const initSocketServer = (server) => {
         methods: ["GET", "POST", "PATCH", "DELETE"],
       },
     });
-    //Happens Whenever a user connects to a socket
+    // Happens Whenever a user connects to a socket
     io.on("connection", (socket) => {
       console.log(`🔗 User ${socket.id} connected to sockets`);
 
@@ -33,8 +33,60 @@ const initSocketServer = (server) => {
       // Add custom socket event handlers here
 
       socket.on("userConnected", async (user) => {
-        //Take that Users informationa and add it to the online Users array
+        // Take that Users informationa and add it to the online Users array
         onlineUsers.push(user);
+      });
+
+      socket.on("friend-request", async (profileId, viewerId) => {
+        //Check if there is already a freiendship record between the two users
+        const friendship = await FriendShip.findOne({
+          where: {
+            [Op.or]: [
+              { user1: profileId, user2: viewerId },
+              { user1: viewerId, user2: profileId },
+            ],
+          },
+        });
+        //If there is, change stats to accepted
+        if (friendship) {
+          if ((friendship.stauts = "pending1"))
+            friendship.update({ status: "accepted" });
+          else if ((friendship.stauts = "pending2"))
+            friendship.update({ status: "accepted" });
+        } else if (!friendship)
+          FriendShip.create({
+            user1: viewerId,
+            user2: profileId,
+            status: "pending2",
+          });
+
+        // Find all friends of the current user
+        const friendsConnected = await FriendShip.findAll({
+          where: {
+            // or operator to check for any friendship where user1 OR user2 is the current user
+            [Op.or]: [
+              { user1: profileId, user2: viewerId },
+              { user1: viewerId, user2: profileId },
+            ],
+            status: "accepted",
+          },
+          // Loads user details for both people in the friendship
+          include: [
+            { model: User, as: "primary" },
+            { model: User, as: "secondary" },
+          ],
+        });
+
+        // maps through friendships to find the friend of the current user
+        const friends = friendsConnected.map((friendship) => {
+          if (friendship.user1 === userId) {
+            return friendship.secondary;
+          } else {
+            return friendship.primary;
+          }
+        });
+
+        socket.emit("friendsList", friends);
       });
     });
   } catch (error) {
