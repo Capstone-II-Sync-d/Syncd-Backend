1a11867624ef36a7990778205f8d90a8a112b8f6
Joseph Collado <sourpina@gmail.com>
2025-08-12 15:06:57 -0400

    #21 Added route for getting user notifications

--- api/notifications.js
+++ api/notifications.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Notification = require("../database/notification");
+const { authenticateJWT } = require("../auth");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -9,6 +10,23 @@ router.get("/", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send({ error: `Error getting all notifications: ${error}` });
+  }
+});
+
+router.get("/me", authenticateJWT, async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const notifs = await Notification.findAll({
+      where: { userId },
+      order: [['createdAt', 'DESC']],
+    });
+    res.status(200).json({
+      message: "Successfully retrieved user's notifications",
+      notifications: notifs,
+    });
+  } catch (error) {
+    console.error("Error fetching user's notifications:", error);
+    res.status(500).json({ error: "Failed to get user's notifications" });
   }
 });
 
