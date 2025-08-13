db9f310c557ed3a4a57a91666e46ca7d8d9d050f
Joseph Collado <sourpina@gmail.com>
2025-08-13 14:19:50 -0400

    #21 Added eager loading with data filtering
    Since notifications are of different types, nested includes were used to include the relevant records, after which the data gets cleaned up before being sent back

--- api/notifications.js
+++ api/notifications.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Notification = require("../database/notification");
 const { authenticateJWT } = require("../auth");
+const { RequestNotification, ReminderNotification, EventNotification, FriendShip, User } = require("../database");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -18,8 +19,58 @@ router.get("/me", authenticateJWT, async (req, res) => {
   try {
     const notifs = await Notification.findAll({
       where: { userId },
+      include: [
+        {
+          model: RequestNotification,
+          required: false,
+          include: [
+            {
+              model: FriendShip,
+              include: [
+                {
+                  model: User,
+                  as: 'primary'
+                },
+                {
+                  model: User,
+                  as: 'secondary'
+                },
+              ],
+            },
+          ],
+        },
+        { model: ReminderNotification, required: false },
+        { model: EventNotification, required: false },
+      ],
       order: [['createdAt', 'DESC']],
     });
+
+    notifs.forEach((notif, index, array) => {
+      const notifInfo = {
+        id: notif.id,
+        time: notif.createdAt,
+        read: notif.read,
+        userId: notif.userId,
+        type: 'blank',
+      }
+      
+      if (notif.request_notification) {
+        notifInfo.type = 'request';
+        notifInfo.status = notif.request_notification.status;
+        const otherUser = notif.request_notification.friendship.primary.id === userId ?
+          notif.request_notification.friendship.secondary : notif.request_notification.friendship.primary;
+        notifInfo.otherUser = {
+          id: otherUser.id,
+          firstName: otherUser.firstName,
+          username: otherUser.username,
+        }
+      }
+      // ELSE IF REMINDER NOTIF
+      // ELSE IF EVENT NOTIF
+
+      array[index] = notifInfo;
+    });
+
     res.status(200).json({
       message: "Successfully retrieved user's notifications",
       notifications: notifs,
