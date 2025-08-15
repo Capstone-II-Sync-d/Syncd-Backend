75e8e9c0ecdefdf2deda950d615855b85631fce0
Guarotav <guarotav@gmail.com>
2025-08-15 15:42:48 -0400

    Changed seed file to not include ids
    Took out ids for calendar items in seed file, and changed ownerId to userId in the event model.

--- database/event.js
+++ database/event.js
@@ -3,55 +3,62 @@ const db = require("./db");
 const Business = require("./business");
 const CalendarItem = require("./calendarItem");
 
-const Event = db.define("event", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+const Event = db.define(
+  "event",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    chatLink: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isUrl: true,
+      },
+    },
+    published: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    businessId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    itemId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
-  chatLink: {
-    type: DataTypes.STRING,
-    allowNull: true,
+  {
     validate: {
-      isUrl: true,
-    },
-  },
-  published: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: false,
-  },
-  businessId: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-  },
-  itemId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  }
-}, {
-  validate: {
-    async isValidEvent() {
-      try {
-        const item = await CalendarItem.findByPk(this.itemId);
-        
-        if (this.published && !item.description)
-          throw new Error("Calendar Item description cannot be null or empty for a published event");
-        
-        if (this.published && !item.location)
-          throw new Error("Calendar Item location cannot be null or empty for a published event");
-        
-        if (!this.businessId)
-          return;
+      async isValidEvent() {
+        try {
+          const item = await CalendarItem.findByPk(this.itemId);
 
-        const business = await Business.findByPk(this.businessId);
-        if (business.ownerId !== item.ownerId)
-          throw new Error("Business owner must match calendar item owner");
-      } catch (error) {
-        throw new Error("Failed to run custom validator:", error);
-      }
+          if (this.published && !item.description)
+            throw new Error(
+              "Calendar Item description cannot be null or empty for a published event"
+            );
+
+          if (this.published && !item.location)
+            throw new Error(
+              "Calendar Item location cannot be null or empty for a published event"
+            );
+
+          if (!this.businessId) return;
+
+          const business = await Business.findByPk(this.businessId);
+          if (business.ownerId !== item.userId)
+            throw new Error("Business owner must match calendar item owner");
+        } catch (error) {
+          throw new Error("Failed to run custom validator:", error);
+        }
+      },
     },
-  },
-});
+  }
+);
 
 module.exports = Event;
